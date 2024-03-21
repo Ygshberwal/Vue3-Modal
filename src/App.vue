41446224b1537a6f7112115302f60b58564ee1e2
Ygshberwal26 <yogeshberwal266@gmail.com>
2024-03-21 14:55:58 +0530

    Initial Commit L:4,5

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h2>{{ title }}</h2>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
+  <!-- header and text can be exported to Modal -->
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header=header :text=text theme="dark" @close="toggleModal"/>
+  </div>
+  <button @click.alt="toggleModal">Open Modal (Alt)</button>
+  
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',
+  components:{ Modal },
+  data(){
+    return{
+      title:'This is my First Vue app :) ',
+      header:"Sign in for the Giveaway!",
+      text:"Grab the Opportunity ASAP",
+      showModal:false
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add("active")   //add a class ative to that element
+      this.$refs.name.focus()
+    },
+    toggleModal(){
+      this.showModal=!this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{dark: theme==='dark'}">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default{
+        props:['header', 'text', 'theme'],
+        methods:{
+            closeModal(){
+                this.$emit('close')
+            }
+        }   
+    }
+</script>
+
+<style>
+.modal{
+    width:400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+.backdrop{
+    top:0%;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+
+}
+h1{
+    color: aqua;
+    border: none;
+    padding: 0;
+}
+.modal.dark{
+    background-color: black;
+    color: white;
+}
+.modal.dark h1{
+    color: white;
+}
+
+</style>
+
+<!-- EXPORT IT TO IT'S PARENT COMPONENT -->
